0e5322bc1468ab24b2adb84ed8a09dafe6e288fb
jimmyhhuang <jimmyhhuang@tencent.com>
2024-04-13 16:08:21 +0800

    --document dashbord项目描述

--- vue-admin-template/src/views/dashboard/index.vue
+++ vue-admin-template/src/views/dashboard/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+  <div class="project-description-container">
+    <div class="project-description-text">
+      <h1>在线教育后台管理系统详情</h1>
+      <p>已经开发的功能，课程管理（CRUD），讲师管理(CRUD)，前台banner管理（CRUD），课程类别管理（CRUD）,数据统计（注册人数，登陆人数统计）</p>
+    </div>
   </div>
 </template>
 
@@ -18,13 +21,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
+.project-description {
   &-container {
-    margin: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    height: 100vh;
+    background-color: #f8f9fa;
   }
+
   &-text {
-    font-size: 30px;
-    line-height: 46px;
+    text-align: center;
+
+    h1 {
+      font-size: 36px;
+      margin-bottom: 20px;
+    }
+
+    p {
+      font-size: 18px;
+      line-height: 1.5;
+    }
   }
 }
 </style>
